c4af28d417da45a9e8a687fd1ba8be026f00bd59
Enge-Olate <marcioslexisolate@live.com>
2024-02-25 14:51:43 -0300

    bit-bit

---
+++ byte.c
@@ -0,0 +1,19 @@
+/* Exercício.; Implemente uma aplicação que mostre os valores contidos em um byte, com 
+    apenas um it igual a 1, movimentando o bit ativo da esquerda para direita.
+    
+    Luiz Damas.
+ */
+#include<stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    unsigned char var = 0xff & 0x80;
+    for (size_t i = 0; i <= 8; i++)
+    {
+        printf("i.:%ld <<>> valor.: %d\n", i, var >> i);
+    }
+    
+    return 0;
+}
